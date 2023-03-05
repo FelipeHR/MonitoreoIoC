de4e70c2eab3f70143d5ddf2bf67c0c7283b689f
chechochecho2 <42447736+chechochecho2@users.noreply.github.com>
2023-03-05 17:57:08 -0300

    Add files via upload

--- communicationNagios.py
+++ communicationNagios.py
@@ -1,9 +1,12 @@
 import communication_pb2_grpc
 import communication_pb2
+import sqlite3
+from sqlite3 import Error
 import grpc
 import subprocess
 import json
 import time
+import sys
 
 credentials = grpc.ssl_channel_credentials(open('certificates/ca.pem','rb').read(),
     open('certificates/clientNagios-key.pem','rb').read(),open('certificates/clientNagios.pem','rb').read())
@@ -13,11 +16,25 @@ ipserver = '192.168.4.84:50051'
 channel = grpc.secure_channel(ipserver,credentials)
 stub = communication_pb2_grpc.CommunicationStub(channel)
 
-def reporte(): 
-    request = communication_pb2.ClientMessage(message = "Necesito Reporte", problem = "Puerto no reconocido", ip = "192.168.0.5")
+
+
+def reporte():
+
+    request = communication_pb2.ClientMessage(message = "Necesito Reporte", problem = sys.argv[2], ip = sys.argv[1])
     reply = stub.NagiosCommunication(request)
     print("La respuesta es: "+reply.message)
 
 
 if __name__ == "__main__":
+
+    
     reporte()
+
+
+
+
+
+
+
+
+
