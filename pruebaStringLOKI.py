cc5ba180a5aaee8ef282092f093f9dd17dfc0327
Felipe Henriquez Ricart <fhenriquez2017@inf.udec.cl>
2023-05-15 16:16:19 -0400

    cambios

--- pruebaStringLOKI.py
+++ pruebaStringLOKI.py
@@ -19,9 +19,13 @@ def formated(texto):
         anterior = i
     return dic
 
-cadena = "20230317T22:09:24Z memoria LOKI: Alert: MODULE: FileScan MESSAGE: FILE: /home/memoria/.config/Code/User/History/-5013641f/7e79.txt SCORE: 480 TYPE: UNKNOWN SIZE: 10665 FIRST_BYTES: 32303233303232385432323a30353a34335a206d / <filter object at 0x7f107729ab90> MD5: cc06d9e1fb2526925ee3d7ecb1cfccfc SHA1: 4a0f73af597296d6dedbd0c7dc7600c2c0de2042 SHA256: c337e13af11fe0b55c50e9a95366d3dc7c6e7d4498b6f6fba3916e246b4935ca CREATED: Thu Mar  9 16:58:54 2023 MODIFIED: Thu Mar  9 16:58:54 2023 ACCESSED: Fri Mar 17 18:23:53 2023 REASON_1: Yara Rule MATCH: EquationDrug_HDDSSD_Op SUBSCORE: 70 DESCRIPTION: EquationDrug - HDD/SSD firmware operation - nls_933w.dll REF: http://securelist.com/blog/research/69203/inside-the-equationdrug-espionage-platform/ AUTHOR: Florian Roth (Nextron Systems) @4nc4p MATCHES: Str1: nls_933w.dllREASON_2: Yara Rule MATCH: webshell_browser_201_3_400_in_JFolder_jfolder01_jsp_leo_ma_warn_webshell_nc_download SUBSCORE: 70 DESCRIPTION: Web Shell REF: - AUTHOR: Florian Roth (Nextron Systems) MATCHES: Str1: UplInfo info = UploadMonitor.getInfo(fi.clientFileName); Str2: long time = (System.currentTimeMillis() - starttime) / 1000l;"
+cadena = "20230510T03:17:18Z memoria LOKI: Warning: MODULE: FileScan MESSAGE: FILE: /home/memoria/Documentos/GitHub/MonitoreoIoC/signature-base/iocs/hash-iocs.txt SCORE: 70 TYPE: UNKNOWN SIZE: 1138146 FIRST_BYTES: 230a23204c4f4b4920435553544f4d204556494c / <filter object at 0x7f72fc49b580> MD5: dd14d9d04d555261c1e3fbfd1dc60833 SHA1: 7278ee797cef3e68c1e8c3b4127d4ba81773ce20 SHA256: defbdb21fd7cf847ba841c47a15329a2f4208768d69ab86ce750c3ad00ab3af1 CREATED: Tue Apr 18 18:45:14 2023 MODIFIED: Tue Apr 18 18:45:14 2023 ACCESSED: Tue May  9 18:32:09 2023 REASON_1: Yara Rule MATCH: EquationDrug_HDDSSD_Op SUBSCORE: 70 DESCRIPTION: EquationDrug - HDD/SSD firmware operation - nls_933w.dll REF: http://securelist.com/blog/research/69203/inside-the-equationdrug-espionage-platform/ AUTHOR: Florian Roth (Nextron Systems) @4nc4p MATCHES: Str1: nls_933w.dll"
 fecha = cadena.split('Z')[0]
-info = cadena.split('Alert: ')[1]
+if len(cadena.split('Alert')) > 1:
+    info = cadena.split('Alert: ')[1]
+else:
+    info = cadena.split('Warning: ')[1]
+
 alert_reason = info.split('REASON_')
 infoseparada = alert_reason[0].split(": ")
 
@@ -39,9 +43,8 @@ for i in alert_reason[1:]:
     dicGeneral["REASON " + str(contadorReason)] = dicReason
     contadorReason += 1
 
-print(dicGeneral)
 import json
-print(type(json.dumps(dicGeneral)))
+print(json.dumps(dicGeneral))
 dicLoki = {}
 dicLoki[fecha] = dicGeneral
 
